4d2567498d9a9c1bd004cd6091b00881a300821e
xs <xiaosheng908080@gmail.com>
2025-11-20 23:08:18 -0500

    1

--- src/pages/LoginWallet.jsx
+++ src/pages/LoginWallet.jsx
@@ -3,30 +3,23 @@ import { useEffect } from "react";
 
 export default function LoginWallet() {
 
-  // 获取递增 UID：从 2001001 开始
   function getNextUid() {
-    // 已生成过的最大 UID
     let lastUid = localStorage.getItem("lastUid");
 
     if (!lastUid) {
-      // 第一次进入网站
       lastUid = 2001000;
     } else {
       lastUid = parseInt(lastUid, 10);
     }
 
     const newUid = lastUid + 1;
-
-    // 记录新的 UID，供下次给别人用
     localStorage.setItem("lastUid", newUid.toString());
-
     return newUid;
   }
 
-  // 生成访客账号
   function generateGuest() {
     return {
-      userId: getNextUid(), // 关键点：从 2001001 开始递增
+      userId: getNextUid(),
       account: "user_" + Math.random().toString(36).substring(2, 10)
     };
   }
@@ -35,7 +28,6 @@ export default function LoginWallet() {
     let userId = localStorage.getItem("userId");
     let account = localStorage.getItem("account");
 
-    // 新用户：创建访客账号
     if (!userId || !account) {
       const guest = generateGuest();
       localStorage.setItem("userId", guest.userId.toString());
@@ -43,8 +35,8 @@ export default function LoginWallet() {
       console.log("Created new guest:", guest);
     }
 
-    // 直接跳首页
-    window.location.replace("/");
+    // ✔ 兼容所有钱包浏览器
+    window.location.href = "/";
   }, []);
 
   return null;
