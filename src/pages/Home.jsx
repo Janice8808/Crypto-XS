d9587de6f1859483165059cb7c5fadd8d8f797a8
xs <xiaosheng908080@gmail.com>
2025-11-22 15:15:26 -0500

    1

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -345,18 +345,16 @@ const bnb = mergedTickers["BNB"] || { price: "--", change: 0 };
 
   <div className="relative flex-1 h-full">
 <div
-  key={marqueeKey}
   className="absolute whitespace-nowrap text-white text-base animate-marquee-RTL"
   style={{
     top: "50%",
-    transform: "translateY(-50%)",   // ⭐ 必须写
     pointerEvents: "none",
     position: "absolute",
-    whiteSpace: "nowrap",
   }}
 >
   {t("Welcome")} Crypto.com
 </div>
+
 
 
   </div>
