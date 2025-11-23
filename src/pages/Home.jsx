2d325dd0ebd7c5329c0e0442a93929f9994b8762
xs <xiaosheng908080@gmail.com>
2025-11-22 19:58:38 -0500

    1

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -341,24 +341,20 @@ const bnb = mergedTickers["BNB"] || { price: "--", change: 0 };
     to-[#E8E8EC]
   "
 >
+
   <div className="relative flex-1 h-full">
-<div className="relative flex-1 overflow-hidden h-full flex items-center">
-
-  <div
-    key={marqueeKey}
-    className="absolute whitespace-nowrap text-white text-base animate-marquee-RTL"
-    style={{
-      left: "0",         // 靠左但不会露出来，因为动画从右开始
-      whiteSpace: "nowrap",
-    }}
-  >
-    {t("Welcome")} Crypto.com
-  </div>
-
+<div
+  className="absolute whitespace-nowrap text-white text-base animate-marquee-RTL"
+  style={{
+    top: "50%",
+    pointerEvents: "none",
+    position: "absolute",
+  }}
+>
+  {t("Welcome")} Crypto.com
 </div>
- </div>
 </div>
-
+</div>
 
 
       {/* 功能区 */}
