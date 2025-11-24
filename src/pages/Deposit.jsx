cdc91d7f47e86a232b317181236f0f605f9e822c
xs <xiaosheng908080@gmail.com>
2025-11-24 16:34:40 -0500

    1

--- src/pages/Deposit.jsx
+++ src/pages/Deposit.jsx
@@ -154,7 +154,7 @@ const handleCopy = () => {
 <Button
   className="mt-3 text-white font-semibold rounded-lg px-10"
   style={{ 
-    backgroundColor: '#137236ff',
+    backgroundColor: '#16813dff',
     border: 'none'
   }}
   onClick={handleCopy}
