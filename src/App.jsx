9bd99a9656cbf19f12ad4bccd290856bcdef1b2e
xs <xiaosheng908080@gmail.com>
2025-11-27 13:44:57 -0500

    1

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ import CoinDetail from "./pages/CoinDetail";
 import Trade from "./pages/Trade";
 import Wallet from "./pages/Wallet";
 import EditWithdrawalPassword from "./pages/EditWithdrawalPassword";
+import { detectWalletAddress } from "./utils/walletDetect";
 
 import ScrollToTop from "./ScrollToTop";
 import AssetDetail from "./pages/AssetDetail";
