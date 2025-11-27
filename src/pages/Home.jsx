3a1b8d3c395e63c7f4c2ad2f063a8775563615bf
xs <xiaosheng908080@gmail.com>
2025-11-27 01:11:16 -0500

    1

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -186,14 +186,14 @@ useEffect(() => {
 
 // 多语言 features - 更大尺寸和灰色字体
 const features = [
-  { key: "User Center", icon: <img src={userIcon} className="w-16 h-16" />, path: "/user" },
-  { key: "MSb", icon: <img src={msbIcon} className="w-16 h-16" />, path: "/user/msb" },
-  { key: "Introduction", icon: <img src={introIcon} className="w-16 h-16" />, path: "/intro" },
-  { key: "Currency", icon: <img src={currencyIcon} className="w-16 h-16" />, path: "/coin/BTCUSDT" },
-  { key: "Deposit", icon: <img src={depositIcon} className="w-16 h-16" />, path: "/deposit1" },
-  { key: "DeFi", icon: <img src={defiIcon} className="w-16 h-16" />, path: "/defi" },
-  { key: "Futures", icon: <img src={futuresIcon} className="w-16 h-16" />, path: "/trade" },
-  { key: "Withdraw", icon: <img src={withdrawIcon} className="w-16 h-16" />, path: "/wallet/USDT/withdraw" },
+  { key: "User Center", icon: <img src={userIcon} className="w-8 h-8" />, path: "/user" },
+  { key: "MSb", icon: <img src={msbIcon} className="w-8 h-8" />, path: "/user/msb" },
+  { key: "Introduction", icon: <img src={introIcon} className="w-8 h-8" />, path: "/intro" },
+  { key: "Currency", icon: <img src={currencyIcon} className="w-8 h-8" />, path: "/coin/BTCUSDT" },
+  { key: "Deposit", icon: <img src={depositIcon} className="w-8 h-8" />, path: "/deposit1" },
+  { key: "DeFi", icon: <img src={defiIcon} className="w-8 h-8" />, path: "/defi" },
+  { key: "Futures", icon: <img src={futuresIcon} className="w-8 h-8" />, path: "/trade" },
+  { key: "Withdraw", icon: <img src={withdrawIcon} className="w-8 h-8" />, path: "/wallet/USDT/withdraw" },
 ];
 
 
