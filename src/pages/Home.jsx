71b59b573918a5bf95fcf96b85d63b8493ba89ee
xs <xiaosheng908080@gmail.com>
2025-11-27 00:21:04 -0500

    1

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -285,7 +285,7 @@ const features = [
 <div className="w-full bg-[#FFB800] px-3 py-0 flex items-center justify-between">
   <div className="flex items-center space-x-2 flex-1">
     <div className="w-8 h-8 rounded-full bg-white flex items-center justify-center">
-      <img src={yonghuIcon} className="w-12 h-12" alt="user" />
+      <img src={yonghuIcon} className="w-6 h-6" alt="user" />
     </div>
 
     <div className="flex-1">
@@ -295,7 +295,7 @@ const features = [
     </div>
   </div>
 
-  <div className="flex items-center justify-end flex-shrink-0"> {/* 使用 flex-shrink-0 防止压缩 */}
+  <div className="flex items-center -space-x-3 flex-shrink-0"> {/* 使用 -space-x-3 负间距 */}
     <button 
       className="relative p-0 bg-transparent" 
       onClick={() => navigate("/notice")}
